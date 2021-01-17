e76d5bcccd0b90ca02c509d9f647ff32a673a87d
Amadhya <eshuanand07@gmail.com>
2021-01-17 23:29:12 +0530

    Added dates for education

--- src/portfolio/education.tsx
+++ src/portfolio/education.tsx
@@ -18,6 +18,7 @@ const education = [
     name: 'Punjab Engineering College',
     location: 'Chandigarh, India',
     result: 'CGPA: 8.1',
+    period: 'Aug, 2016 - Jun, 2020',
     extra:
       'Member of NSS, Rotaract Club and Robotics society. Was part of the publicity and discipline team of Pecfest.',
   },
@@ -26,6 +27,7 @@ const education = [
     subTitle: 'Non Medical',
     name: 'GMSSS-35',
     location: 'Chandigarh, India',
+    period: 'Aug, 2014 - Mar, 2016',
     result: '91.8%',
   },
   {
@@ -34,6 +36,7 @@ const education = [
     name: 'Ryan International School',
     location: 'Chandigarh, India',
     result: 'CGPA: 9.8',
+    period: 'March 2014',
   },
 ];
 
@@ -44,8 +47,8 @@ const Education = () => (
       &nbsp; Education
     </HeaderWrapper>
     <Separator />
-    {education.map(({ title, subTitle, name, location, result, extra }) => (
-      <DetailCard title={title} key={name}>
+    {education.map(({ title, subTitle, name, location, result, extra, period }) => (
+      <DetailCard title={title} key={name} period={period}>
         <div>
           <HeaderWrapper level={4} color="black">
             {subTitle}
