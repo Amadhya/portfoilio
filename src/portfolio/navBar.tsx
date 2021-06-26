388cc53e68f4ca00917418765eec4feaef4a45fd
Amadhya <eshuanand07@gmail.com>
2021-06-26 19:25:32 +0530

    scroll interaction
    - Highlight which section user is on in the navbar.

--- src/portfolio/navBar.tsx
+++ src/portfolio/navBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
 import { Navicon } from '@styled-icons/evil/Navicon';
@@ -12,11 +12,20 @@ type WrapperProps = {
   changeBg: Number;
 };
 
-const ButtonWrapper = styled(Button)`
+type ButtonProps = {
+  isactive: string;
+};
+
+const ButtonWrapper = styled(Button)<ButtonProps>`
   color: white !important;
   &: hover {
     color: ${Colors.CERULEAN} !important;
   }
+  ${({ isactive }) =>
+    isactive === 'true' &&
+    css`
+      border-bottom: 2px solid ${Colors.CERULEAN} !important;
+    `};
 `;
 
 const Wrapper = styled.div<WrapperProps>`
@@ -95,47 +104,84 @@ const links = [
   },
 ];
 
+const getDimensions = (ele: HTMLElement | null) => {
+  if (!ele) {
+    return {};
+  }
+  const { height } = ele.getBoundingClientRect();
+  const {offsetTop} = ele;
+  const offsetBottom = offsetTop + height;
+
+  return {
+    height,
+    offsetTop,
+    offsetBottom,
+  };
+};
+
 const NavBar = () => {
   const [changeBg, setChangeBg] = useState(false);
   const [open, setOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState('home');
+  const headerRef = useRef(null);
 
   const onClick = useCallback((id: string) => {
     const scrollY = document.getElementById(id)?.offsetTop;
 
-    if (scrollY) {
+    if (scrollY || scrollY === 0) {
       window.scroll({
-        top: scrollY - 60,
+        top: Math.max(scrollY - 40, 0),
         behavior: 'smooth',
       });
     }
   }, []);
 
   const handleScroll = useCallback(() => {
-    const aboutOffsetTop = document.getElementById('about')?.offsetTop;
-    const offsetY = window.pageYOffset;
+    const { height: headerHeight = 0 } = getDimensions(headerRef.current);
+    const scrollPosition = window.scrollY + headerHeight;
+    const aboutEle = document.getElementById('about');
+    const { offsetTop: aboutOffsetTop } = getDimensions(aboutEle);
 
-    if (aboutOffsetTop && offsetY < aboutOffsetTop) {
+    if (aboutOffsetTop && scrollPosition < aboutOffsetTop) {
       setChangeBg(false);
-    } else if (aboutOffsetTop && offsetY >= aboutOffsetTop && !changeBg) {
+    } else if (aboutOffsetTop && scrollPosition >= aboutOffsetTop && !changeBg) {
       setChangeBg(true);
     }
-  }, [changeBg]);
+
+    const { id: selectedId } =
+      links.find(({ id }) => {
+        const ele = document.getElementById(id);
+
+        if (ele) {
+          const { offsetBottom = 0, offsetTop = 0 } = getDimensions(ele);
+
+          return scrollPosition > offsetTop - 80 && scrollPosition < offsetBottom;
+        }
+
+        return false;
+      }) || {};
+
+    if (selectedId && selectedId !== activeTab) {
+      setActiveTab(selectedId);
+    }
+  }, [changeBg, headerRef, setActiveTab, activeTab]);
 
   useEffect(() => {
     const doc = document.getElementById('home');
 
     if (doc) {
+      handleScroll();
+
       window.addEventListener('scroll', handleScroll);
 
       return () => window.removeEventListener('scroll', handleScroll);
     }
 
-    return undefined;
-    // eslint-disable-next-line
-  }, []);
+    return () => {};
+  }, [handleScroll]);
 
   return (
-    <Wrapper changeBg={Number(changeBg)}>
+    <Wrapper changeBg={Number(changeBg)} ref={headerRef}>
       {open ? (
         <CloseOutlineIcon size={28} onClick={() => setOpen(false)} />
       ) : (
@@ -143,7 +189,12 @@ const NavBar = () => {
       )}
       <RowWrapper align="middle" justify="space-around" visible={open ? 1 : 0}>
         {links.map(({ id, title }) => (
-          <ButtonWrapper type="link" key={id} onClick={() => onClick(id)}>
+          <ButtonWrapper
+            type="link"
+            key={id}
+            onClick={() => onClick(id)}
+            isactive={(activeTab === id).toString()}
+          >
             {title}
           </ButtonWrapper>
         ))}
