e55e0f3ac789cadbc6da0991e3e97b2115769a84
Amadhya Anand <35623760+Amadhya@users.noreply.github.com>
2022-10-16 22:47:05 +0200

    Updated Work Exp & Location

--- src/constants/about.tsx
+++ src/constants/about.tsx
@@ -1,10 +1,10 @@
 const ABOUT = {
   name: 'Amadhya Anand',
-  address: 'Chandigarh, India',
-  languages: ['English', 'Hindi', 'Punjabi'],
+  address: 'Amsterdam, Netherlands',
+  languages: ['English', 'Hindi'],
   email: 'amadhya.anand@gmail.com',
   info:
-    'Young, energetic, and enthusiastic engineer, who wants to pursue a challenging and growing career as a Software Developer with the aim of holistic development and organizational growth. By the way, helping out people is something that I relish the most. In my leisure time, I like to watch TV series.',
+    'Young, energetic, and enthusiastic engineer, who wants to pursue a challenging and growing career as a Software Developer with the aim of holistic development and organizational growth. By the way, helping out people is something that I relish the most. In my leisure time, I like to cook and watch TV series.',
   resume: 'https://drive.google.com/file/d/1480Lmh0mgIYZ0hRA8Fmh_w1ASu-OL7YH/view?usp=sharing',
   profession: 'Software Development Engineer',
   quote:
--- src/constants/workExp.tsx
+++ src/constants/workExp.tsx
@@ -1,15 +1,33 @@
 const WORKEXP = [
   {
-    period: 'July, 2020 - Present',
+    period: 'September, 2022 - Present',
+    title: 'Booking.com',
+    subTitle: 'Software Engineer',
+    location: 'Amsterdam, Netherlands',
+    desc: [],
+  },
+  {
+    period: 'April, 2022 - August, 2022',
+    title: 'Cuemath',
+    subTitle: 'Senior Software Developer',
+    location: 'Bangalore, India',
+    desc: [
+      'Integrating 3rd Party SDKs for Group Call between teacher, student and relationship manager.',
+      'Integrating 3rd Party SDKs for Audio Video Call and Screen Share between student and teacher.',
+    ],
+  },
+  {
+    period: 'July, 2020 - March, 2022',
     title: 'Cuemath',
     subTitle: 'Software Developer',
     location: 'Bangalore, India',
     desc: [
       'Develop and maintain features for the online teaching platform for teachers and students.',
       'Working on post-processing of Audio Video Call recordings and improving the performance of the call during the class.',
-      'Worked on improving the overall demo experience of teachers and students.',
       'Worked on improving the classroom experience for teachers.',
+      'Worked on improving the overall demo experience of teachers and students by developing features like a presentation by a teacher.',
       'Developed SDKs for in-house analytics system.',
+      "To help students, teachers, and parents get insight into student's monthly performance, developed a monthly progress report. Technologies used: React, D3.js and Flask.",
     ],
   },
   {
